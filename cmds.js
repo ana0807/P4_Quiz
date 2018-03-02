3a7ac872c4a4aa95951a53951061cb67816a1894
Ana Martinez <ana.martinez.martin@alumnos.upm.es>
2018-03-02 13:39:07 +0100

    Primera version

---
+++ cmds.js
@@ -0,0 +1,206 @@
+/**
+ * Created by ana.martinez.martin on 1/03/18.
+ */
+
+
+const model=require('./model');
+
+const {log ,biglog,errorlog,colorize}=require('./out');
+
+
+
+exports.helpCmd=rl=>{
+    log("Comandos:");
+    log("h|help - Muestra esta ayuda.");
+    log("list - Listar los quizzes existentes.");
+    log("show <id> - Muestra la pregunta y la respuesta del quiz indicado.");
+    log("add - Añadir un nuevo quiz interactivamente." );
+    log("delete<id> - Borrar el quiz indicado.");
+    log("edit <id> - Editar el quiz indicado.");
+    log("test <id> - Probar el quiz indicado.");
+    log("p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+    log("credits - Créditos.");
+    log("q|quit - Salir del programa.");
+    rl.prompt();
+};
+
+
+
+exports.quitCmd=rl=>{
+    rl.close();
+};
+
+
+exports.addCmd=rl=>{
+    //log('Añadir un nuevo quiz','red');
+    rl.question(colorize('Introduzca una pregunta: ', 'red'),question =>{
+
+        rl.question(colorize('Introduzca la respuesta ', 'red'),answer =>{
+
+            model.add(question,answer);
+            log(`${colorize('Se ha añadido','magenta')}: ${question} ${colorize('=>','magenta')} ${answer}`);
+
+            rl.prompt();
+        });
+    });
+
+};
+
+exports.listCmd=rl=>{
+    //log('Listar todos los quizzes existentes.','red');
+    model.getAll().forEach((quiz,id)=>{
+
+        log(`[${colorize(id,'magenta')}]: ${quiz.question}`);
+    });
+
+
+
+    rl.prompt();
+};
+
+
+exports.showCmd=(rl,id)=>{
+    //log('Mostrar el quiz indicado.','red');
+    if (typeof id ==="undefined"){
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try{
+            const quiz = model.getByIndex(id);
+            log(`[${colorize(id,'magenta')}]: ${quiz.question} ${colorize('=>','magenta')} ${quiz.answer}`);
+        } catch(error){
+            errorlog(error.message);
+        }
+
+    }
+
+
+    rl.prompt();
+};
+
+
+exports.testCmd=(rl,id)=> {
+    if (typeof id === "undefined") {
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    } else {
+        try {
+            const quiz = model.getByIndex(id);
+
+            rl.question(colorize(`${quiz.question}`,'red') , resp=>{
+                if (resp === quiz.answer) {
+                    biglog('CORRECTO', 'green');
+
+                } else {
+                    biglog('INCORRECTO', 'red');
+                }
+
+            });rl.prompt();
+
+        } catch (error) {
+            errorlog(error.message);
+            rl.prompt();
+        }
+
+    }
+};
+   // log('Probar el quiz indicado.','red');
+    //rl.prompt();
+
+
+
+
+
+exports.deleteCmd=(rl,id)=>{
+    if (typeof id ==="undefined"){
+        errorlog(`Falta el parámetro id.`);
+    } else {
+        try{
+            model.deleteByIndex(id);
+        } catch(error){
+            errorlog(error.message);
+        }
+
+    }
+
+
+    rl.prompt();
+};
+
+
+exports.editCmd=(rl,id)=>{
+    if (typeof id=== "undefined"){
+        errorlog(`Falta el parámetro id.`);
+        rl.prompt();
+    }else {
+        try {
+            const quiz =model.getByIndex(id);
+
+            process.stdout.isTTY && setTimeout(()=>{rl.write(quiz.question)},0);
+            rl.question(colorize('Introduzca una pregunta:','red'),question =>{
+
+                process.stdout.isTTY && setTimeout(()=>{rl.write(quiz.answer)},0);
+                rl.question(colorize('Introduzca la respuesta:','red'),answer=>{
+                    model.update(id,question,answer);
+                    log(`Se ha cambiado el quiz ${colorize(id,'magenta')} por: ${question} ${colorize('=>', 'magenta')} ${colorize(answer,'magenta')}`);
+                    rl.prompt();
+                    });
+            });
+
+        }catch (error){
+            errorlog(error.message);
+            rl.prompt();
+        }
+    }
+
+};
+
+
+exports.playCmd=rl=>{
+    let score = 0;
+
+    let toBeResolved = [];
+
+    let i=0;
+
+    for (i= 0; i<model.getAll().length;i++){
+        toBeResolved[i]=i;
+    }
+
+    const playOne = ()=>{
+    if (toBeResolved===null) {
+        errorlog(`No hay preguntas`);
+        rl.prompt();
+
+    }else {
+        let indice = Math.floor(Math.random(toBeResolved.length))
+
+        let quiz= toBeResolved[indice];
+
+        rl.question(colorize(`${quiz.question}`,'red'), resp => {
+            if (resp===quiz.answer){
+             log(`Llevas acertadas ${colorize(score++,'magenta')}`);
+             score++;
+             playOne();
+
+            } else{
+            log(`Resultado incorrecto. Totales correctas: ${colorize(score, 'magenta')}`);
+        }
+
+        });rl.prompt();
+    }
+}
+
+playOne();
+
+   // log('Jugar.','red');
+   // rl.prompt();
+};
+
+
+exports.creditsCmd=rl=>{
+    log('Autores de la practica:','red');
+    log('Ana Martínez','green');
+    log('Selene Pérez','green');
+    rl.prompt();
+};
+
